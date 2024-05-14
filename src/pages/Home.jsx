2d20c874825537e0b7c872664028bf9e6a1ba3fc
charmiainemimie <charmainemangorima@gmail.com>
2024-05-14 10:23:00 +0200

    ...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
   return (
     <div className="grid grid-cols-2 lg:grid-cols-3 gap 6">
     
-      <div className="col-span-1 lg:col-span-2">
+      <div className="col-span-2">
         {workouts && workouts.map((workout)=>(
           <WorkoutDetails key={workout.id} workout={workout}/>
         ))}
