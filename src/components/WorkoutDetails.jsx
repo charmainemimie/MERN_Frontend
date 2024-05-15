b78c2f1e3a0ff1d3e989746316ce156671db1513
charmiainemimie <charmainemangorima@gmail.com>
2024-05-15 14:58:04 +0200

    delete button

--- src/components/WorkoutDetails.jsx
+++ src/components/WorkoutDetails.jsx
@@ -2,13 +2,20 @@
 
 
 const WorkoutDetails = ({workout}) => {
+
+  const handleClick=()=>{
+
+  }
   return (
    <div className='relative bg-[#fff] border border-2 m-4 p-4 shadow-md '>
-    <div className='py-4'>
-    <h4 className='font-bold mb-3 text-lg text-blue-700'>{workout.title}</h4>
+    <div className='py-4 flex items-center justify-between'>
+      <div>  <h4 className='font-bold mb-3 text-lg text-blue-700'>{workout.title}</h4>
     <p className='m-0 text-sm'><span className='font-bold'>Load (kg): </span>{workout.load}</p>
     <p className='m-0 text-sm'><span className='font-bold'>Reps: </span>{workout.reps}</p>
-    <p >{workout.createdAt}</p>
+    <p >{workout.createdAt}</p></div>
+  
+
+    <div><span onClick={handleClick} className="border py-2 px-2 rounded-lg bg-blue-700 text-white mr-10 font-medium hover:cursor-pointer">Delete</span></div>
     </div>
    </div>
   )
