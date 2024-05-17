a85306399fa44afb89eb4bc1f4c55bb419f9f8f6
charmiainemimie <charmainemangorima@gmail.com>
2024-05-17 13:48:35 +0200

    signup form

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import MainLayout from "./components/layout/MainLayout";
 import LoginForm from "./components/auth/LoginForm";
+import SignupForm from "./components/auth/SignupForm";
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route index element={<LoginForm />} />
+      <Route path='/signup' element={<SignupForm/>}/>
         <Route path='/' element={<MainLayout/>}>
         <Route path="home" element={<Home/>} />
         </Route>
--- src/components/auth/SignupForm.jsx
+++ src/components/auth/SignupForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import {Link} from 'react-router-dom'
-const LoginForm = () => {
+import { Link } from "react-router-dom";
+
+const SignupForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
@@ -8,7 +9,7 @@ const LoginForm = () => {
   return (
     <div className="flex justify-center items-center h-screen ">
       <form className="max-w-full w-96 bg-gray-100 p-4 rounded">
-        <h4 className="pb-4 text-xl">Login to Mimie Workouts</h4>
+        <h4 className="pb-4 text-xl">Signup to Mimie Workouts</h4>
         <div className="mb-4">
           <label className="form-control">
             <div className="label">
@@ -49,10 +50,10 @@ const LoginForm = () => {
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Login
+          Signup
         </button>
             <div className="pt-4">
-                <p>Do not have an account yet? <Link to='/signup' className="text-blue-700 font-bold">Signup</Link></p>
+                <p>Already have an account? <Link to='/' className="text-blue-700 font-bold">Login</Link></p>
             </div>
         {error && <div className="error">{error}</div>}
       </form>
@@ -60,4 +61,4 @@ const LoginForm = () => {
   );
 };
 
-export default LoginForm;
+export default SignupForm;
